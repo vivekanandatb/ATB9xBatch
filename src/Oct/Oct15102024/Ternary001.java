999b356c4e69b9ddbad2dc0e47a9481434ab9831
vivekanandatb <vivek.g1691@gmail.com>
2024-10-15 15:13:22 +0530

    commit new class

---
+++ src/Oct/Oct15102024/Ternary001.java
@@ -0,0 +1,13 @@
+package Oct.Oct15102024;
+
+public class Ternary001 {
+
+        public static void main(String[] args) {
+            // - result = condition ? expression1 : expression2;
+            int number = -5;
+            String result = number > 0 ? "Positive" : "Negative";
+            System.out.println(result);
+
+        }
+}
+
---
+++ src/Oct/Oct15102024/Ternary002.java
@@ -0,0 +1,14 @@
+package Oct.Oct15102024;
+
+public class Ternary002 {
+
+        public static void main(String[] args) {
+            // - result = condition ? expression1 : expression2;
+            int age = 18;
+            String Result = age>18 ? "Allowed to vote" :"Not allowed to vote";
+            System.out.println(Result);
+
+
+        }
+}
+
